562d670b5aa0fc25a8b4a74d11904acf01bf252b
animusicl <animusicl@gmail.com>
2021-06-27 22:53:18 -0300

    hystrix config

--- shopping-service/src/main/java/com/academy/digitallab/store/shoppingservice/client/CustomerHystrixFallbackFactory.java
+++ shopping-service/src/main/java/com/academy/digitallab/store/shoppingservice/client/CustomerHystrixFallbackFactory.java
@@ -3,15 +3,18 @@ package com.academy.digitallab.store.shoppingservice.client;
 import com.academy.digitallab.store.shoppingservice.model.Customer;
 import com.academy.digitallab.store.shoppingservice.model.Region;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 
+@Component
 public class CustomerHystrixFallbackFactory implements CustomerClient {
+
     @Override
     public ResponseEntity<Customer> getCustomer(Long id) {
         Customer customer = Customer.builder()
                 .dni("XXXXXXXX")
-                .fistName("Facu el Ssr mas lindo e inteligente")
-                .lastName("del Condado Sanchez")
-                .email("facubb@gmail.com")
+                .fistName("what")
+                .lastName("ever")
+                .email("whatever@gmail.com")
                 .photoUrl("someurl")
                 .region(new Region())
                 .state("created")
